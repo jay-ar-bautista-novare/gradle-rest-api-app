59995fb87cee7feed8c18439c80d3e2c1aa85fd3
Rodexter Malinao <rodexter.malinao@novare.com.hk>
2022-10-07 18:18:29 +0800

    test123

--- src/main/java/com/sofa/pilot/controller/TeacherController.java
+++ src/main/java/com/sofa/pilot/controller/TeacherController.java
@@ -9,7 +9,7 @@ import com.sofa.pilot.dto.TeacherOutput;
 import com.sofa.pilot.service.CrudService;
 
 @RestController
-@RequestMapping(path = "/api/teachersTest2")
+@RequestMapping(path = "/api/teachersTest123")
 public class TeacherController extends BaseCrudController<TeacherInput, TeacherOutput> {
 
 	@Autowired
